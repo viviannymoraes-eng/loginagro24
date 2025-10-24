2c3a986cf296688ca07f180bf2e88dc1468ac674
unknown <vivianny.moraes@aluno.senai.br>
2025-10-23 21:52:45 -0400

    criando a tela de login

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,15 @@
+import {View, Text, StyleSheet} from 'react-native';
+export default function Login(){
+    return(
+        <View style={ style.container}>
+            <Text>
+                Só Alegria!
+            </Text>
+        </View>
+    )
+}
+const style = StyleSheet.create({
+    container:{
+        flex:1,
+    }
+});
